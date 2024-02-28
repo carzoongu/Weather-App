fc33a63a6a09c86815aa1d912bfc55e1a321a0ab
Benson Kazungu <108935596+carzoongu@users.noreply.github.com>
2024-02-28 13:33:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const API_KEY = "da94dd02dae9d736b3977b0bb6b3b957"; // API key for OpenWeatherMap API
+const API_KEY = "#"; // API key for OpenWeatherMap API
 
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card
