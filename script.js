188036b2d565d13d90d6b50faab692cedf78a83a
Benson Kazungu <108935596+carzoongu@users.noreply.github.com>
2024-02-29 09:37:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const API_KEY = "#";
+const API_KEY = " # ";
 
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card
